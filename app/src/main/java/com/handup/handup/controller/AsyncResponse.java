97b2f77a87b3dc8492709539e8752f7b2d5c6db3
c4goldsw <christopher.goldsworthy@outlook.com>
2016-01-30 20:56:45 -0500

    Wiping repository because it's messed up

--- app/src/main/java/com/handup/handup/controller/AsyncResponse.java
+++
@@ -1,8 +0,0 @@
-package com.handup.handup.controller;
-
-/**
- * Created by Christopher on 1/4/2016.
- */
-public interface AsyncResponse {
-    public void returnResponse(String data);
-}
--- app/src/main/java/com/handup/handup/view/MenuListAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.handup.handup.view;
-
-import android.support.v7.widget.RecyclerView;
-
-/**
- * Created by Christopher on 1/10/2016.
- */
-public class MenuListAdapter {
-}
